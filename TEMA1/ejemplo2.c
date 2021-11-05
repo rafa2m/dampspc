9f4eaf93ef4cd3c6145faad37bf6498b3b6b2986
rafa2m <rafaelmendez@iesantoniogala.es>
2021-11-05 09:21:35 +0100

    Se llama al comando ls desde la función execl

--- TEMA1/ejemplo2.c
+++ TEMA1/ejemplo2.c
@@ -1,2 +1,9 @@
 #include <unistd.h>
 #include <stdio.h>
+//Se llama al comando ls desde la función execl
+int main(){
+    printf("Llamando al comando ls -l con la función execl()");
+    printf("Lista de archivos del directorio actual:\n");
+    execl("/bin/ls", "ls","l",(char *)NULL);
+    printf("Aquí no llega\n");
+}
