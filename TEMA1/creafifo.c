955d3dd2e3e4b5990c912d48d8f233a4a13bd5a5
rafa2m <rafaelmendez@iesantoniogala.es>
2021-11-05 09:29:04 +0100

    Ejemplo de proceso que crea un FIFO para comunicarse con otro proceso no relacionado

--- TEMA1/creafifo.c
+++ TEMA1/creafifo.c
@@ -4,7 +4,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h> 
-
+//Ejemplo de proceso que crea un FIFO para comunicarse con otro proceso no relacionado
 int main(void)
 {
   int fp;  
