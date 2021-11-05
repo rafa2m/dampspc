c187591f177edc9bd31e68c2ebdaf454c468424e
rafa2m <rafaelmendez@iesantoniogala.es>
2021-11-05 09:20:24 +0100

    Se elimina el ejemplo c++

--- TEMA1/ejemplo2.cpp
+++
@@ -1,9 +0,0 @@
-#include <unistd.h>
-#include <stdio.h>
-
-int main(){
-    printf("Llamando al comando ls -l con la función execl()");
-    printf("Lista de archivos del directorio actual:\n");
-    execl("/bin/ls", "ls","l",(char *)NULL);
-    printf("Aquí no llega\n");
-}
